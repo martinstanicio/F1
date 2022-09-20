b033f45dd934e3eeafd9a277b52c95cd58140bc0
Martín Stanicio <martuytimo@gmail.com>
2022-09-19 22:17:56 -0300

    commit inicial

---
+++ autos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Formula 1 | Autos</title>
+    <link rel="stylesheet" href="./styles.css" />
+  </head>
+
+  <body>
+    <nav>
+      <img src="./img/logo.svg" alt="logo" />
+
+      <ul>
+        <li><a href="./index.php">Inicio</a></li>
+        <li><a href="./2022.php">2022</a></li>
+        <li><a href="./pilotos.php">Pilotos</a></li>
+        <li><a href="./autos.php">Autos</a></li>
+        <li><a href="./contacto.php">Contacto</a></li>
+      </ul>
+    </nav>
+
+    <header>
+      <h1>Autos icónicos</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </header>
+
+    <main>
+      <section class="container"></section>
+    </main>
+
+    <footer>
+      <p>Hecho por MS & AR @ 2022</p>
+      <ul>
+        <li><a href="https://www.facebook.com/Formula1">Facebook</a></li>
+        <li><a href="https://twitter.com/f1">Twitter</a></li>
+        <li><a href="https://www.instagram.com/f1/">Instagram</a></li>
+        <li><a href="https://www.youtube.com/F1">YouTube</a></li>
+      </ul>
+    </footer>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Formula 1</title>
+    <link rel="stylesheet" href="./styles.css" />
+  </head>
+
+  <body>
+    <nav>
+      <img src="./img/logo.svg" alt="logo" />
+
+      <button>A</button>
+      <ul class="links">
+        <li><a href="./index.php">Inicio</a></li>
+        <li><a href="./2022.php">2022</a></li>
+        <li><a href="./pilotos.php">Pilotos</a></li>
+        <li><a href="./autos.php">Autos</a></li>
+        <li><a href="./contacto.php">Contacto</a></li>
+      </ul>
+    </nav>
+
+    <header>
+      <img src="./img/hero.jpg" alt="imagen de encabezado" />
+    </header>
+
+    <main>
+      <section class="container">
+        <h2>Historia y origen</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam id
+          scelerisque sem. Proin consequat faucibus felis non tristique.
+          Phasellus non mauris id sem facilisis elementum. Aenean porta purus
+          non gravida scelerisque. Aenean rutrum tellus vitae ante dignissim
+          pulvinar. Proin ornare ipsum nec tristique fringilla. Fusce quis
+          aliquam ipsum. Fusce tincidunt magna lacus, scelerisque rhoncus dui
+          pretium a. Pellentesque justo elit, bibendum in lorem ut, consectetur
+          lacinia nunc.
+        </p>
+      </section>
+      <section class="container">
+        <h2>Datos curiosos</h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+      </section>
+    </main>
+
+    <footer>
+      <p>Hecho por MS & AR @ 2022</p>
+      <ul>
+        <li><a href="https://www.facebook.com/Formula1">Facebook</a></li>
+        <li><a href="https://twitter.com/f1">Twitter</a></li>
+        <li><a href="https://www.instagram.com/f1/">Instagram</a></li>
+        <li><a href="https://www.youtube.com/F1">YouTube</a></li>
+      </ul>
+    </footer>
+  </body>
+</html>
