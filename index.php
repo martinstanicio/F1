040e84e0e1e4c7222284527a4875b2977938744b
Martín Stanicio <martuytimo@gmail.com>
2022-10-25 22:09:07 -0300

    modelo Piloto creado y añadido a la database

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once "config/db.php";
 require_once "controller/controller.php";
 require_once "model/model.php";
+require_once "model/piloto.php";
 
 $controller = new Controller();
 $controller->template();
---
+++ views/components/piloto.php
@@ -0,0 +1,6 @@
+<a class="pilotos__piloto" href="<?php echo $link; ?>" style="<?php echo $bg; ?>" target="blank">
+    <div class="text">
+        <h2><?php echo $name . " " . $surname; ?></h2>
+        <p><?php echo $titles . "T"; ?></p>
+    </div>
+</a>
