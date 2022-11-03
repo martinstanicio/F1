2e14c872dd769bf2f7299a0931623f5b3dee8c46
Martín Stanicio <martuytimo@gmail.com>
2022-11-03 20:47:35 -0300

    fix (formato)

--- views/inicio.php
+++ views/inicio.php
@@ -3,41 +3,23 @@
 </header>
 <section class="container">
     <h2>Historia y origen</h2>
-    <p>El inicio de la Fórmula 1 moderna se remonta al año <a
-            href="https://es.wikipedia.org/wiki/Temporada_1950_de_F%C3%B3rmula_1" title="Temporada 1950 de Fórmula 1"
-            target="_blank">1950</a>, en el que participaron <a
-            href="https://es.wikipedia.org/wiki/Temporada_1950_de_F%C3%B3rmula_1" title="Escuderías"
-            target="_blank">escuderías</a> como <a href="https://es.wikipedia.org/wiki/Scuderia_Ferrari"
-            title="Scuderia Ferrari" target="_blank">Ferrari</a>, <a
-            href="https://es.wikipedia.org/wiki/Alfa_Romeo_en_F%C3%B3rmula_1" title="Alfa Romeo en Fórmula 1"
-            target="_blank">Alfa Romeo</a> y <a href="https://es.wikipedia.org/wiki/Maserati_en_F%C3%B3rmula_1"
-            title="Maserati en Fórmula 1" target="_blank">Maserati</a>. Algunas fueron reemplazadas por otras nuevas
+    <p>
+        El inicio de la Fórmula 1 moderna se remonta al año <a href="https://es.wikipedia.org/wiki/Temporada_1950_de_F%C3%B3rmula_1" title="Temporada 1950 de Fórmula 1" target="_blank">1950</a>, en el que participaron <a href="https://es.wikipedia.org/wiki/Temporada_1950_de_F%C3%B3rmula_1" title="Escuderías" target="_blank">escuderías</a> como <a href="https://es.wikipedia.org/wiki/Scuderia_Ferrari" title="Scuderia Ferrari" target="_blank">Ferrari</a>, <a href="https://es.wikipedia.org/wiki/Alfa_Romeo_en_F%C3%B3rmula_1" title="Alfa Romeo en Fórmula 1" target="_blank">Alfa Romeo</a> y <a href="https://es.wikipedia.org/wiki/Maserati_en_F%C3%B3rmula_1" title="Maserati en Fórmula 1" target="_blank">Maserati</a>. Algunas fueron reemplazadas por otras nuevas
         como
-        <a href="https://es.wikipedia.org/wiki/McLaren" title="McLaren" target="_blank">McLaren</a>, <a
-            href="https://es.wikipedia.org/wiki/Williams_Grand_Prix_Engineering" title="Williams Grand Prix Engineering"
-            target="_blank">Williams</a>, <a href="https://es.wikipedia.org/wiki/Red_Bull_Racing"
-            title="Red Bull Racing" target="_blank">Red Bull</a> y otras que volvieron como <a
-            href="https://es.wikipedia.org/wiki/Mercedes-Benz_en_F%C3%B3rmula_1" title="Mercedes-Benz en Fórmula 1"
-            target="_blank">Mercedes</a> que se han alzado varias veces con el <a
-            href="https://es.wikipedia.org/wiki/Campeonato_Mundial_de_Constructores_de_F%C3%B3rmula_1"
-            title="Campeonato Mundial de Constructores de Fórmula 1" target="_blank">Campeonato Mundial de
-            Constructores</a>. Por su parte, los pilotos deben contar con la <a
-            href="https://es.wikipedia.org/wiki/Superlicencia_de_la_FIA" title="Superlicencia de la FIA"
-            target="_blank">superlicencia de la FIA</a> para competir, que se obtiene por los resultados en otros
+        <a href="https://es.wikipedia.org/wiki/McLaren" title="McLaren" target="_blank">McLaren</a>, <a href="https://es.wikipedia.org/wiki/Williams_Grand_Prix_Engineering" title="Williams Grand Prix Engineering" target="_blank">Williams</a>, <a href="https://es.wikipedia.org/wiki/Red_Bull_Racing" title="Red Bull Racing" target="_blank">Red Bull</a> y otras que volvieron como <a href="https://es.wikipedia.org/wiki/Mercedes-Benz_en_F%C3%B3rmula_1" title="Mercedes-Benz en Fórmula 1" target="_blank">Mercedes</a> que se han alzado varias veces con el <a href="https://es.wikipedia.org/wiki/Campeonato_Mundial_de_Constructores_de_F%C3%B3rmula_1" title="Campeonato Mundial de Constructores de Fórmula 1" target="_blank">Campeonato Mundial de
+            Constructores</a>. Por su parte, los pilotos deben contar con la <a href="https://es.wikipedia.org/wiki/Superlicencia_de_la_FIA" title="Superlicencia de la FIA" target="_blank">superlicencia de la FIA</a> para competir, que se obtiene por los resultados en otros
         campeonatos.
     </p>
 </section>
 <section class="container">
     <h2>Neumaticos</h2>
     <!-- <img src="./img/tyres.png" alt="Neumáticos"> -->
-    <p>Debido a la decisión de la <a
-            href="https://es.wikipedia.org/wiki/F%C3%A9d%C3%A9ration_Internationale_de_l%27Automobile"
-            title="Fédération Internationale de l'Automobile" target="_blank">FIA</a> de imponer un único fabricante de
+    <p>
+        Debido a la decisión de la <a href="https://es.wikipedia.org/wiki/F%C3%A9d%C3%A9ration_Internationale_de_l%27Automobile" title="Fédération Internationale de l'Automobile" target="_blank">FIA</a> de imponer un único fabricante de
         neumáticos en el Mundial a partir de 2008, Michelin anunció que dejaba la Fórmula 1 después del campeonato de
         2006. De este modo, Bridgestone fue el único proveedor de neumáticos desde el año 2007 hasta el 2010.
         Bridgestone anunció su retirada de la Fórmula 1 al acabar el campeonato de 2010. A partir de entonces, la
-        compañía industrial italiana <a href="https://es.wikipedia.org/wiki/Pirelli" title="Pirelli"
-            target="_blank">Pirelli</a> tomó el relevo, siendo el único proveedor hasta la actualidad.
+        compañía industrial italiana <a href="https://es.wikipedia.org/wiki/Pirelli" title="Pirelli" target="_blank">Pirelli</a> tomó el relevo, siendo el único proveedor hasta la actualidad.
     </p>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/wsCriICZ-nA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <article>
