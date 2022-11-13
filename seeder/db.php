13a7dbdf5127082f94cf17c3d5273362bab39336
Martín Stanicio <martuytimo@gmail.com>
2022-11-13 18:55:20 -0300

    fix (seeder)

--- seeder/db.php
+++ seeder/db.php
@@ -6,7 +6,7 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = mysqli_connect("localhost", "root", "");
 
 $user = "CREATE USER IF NOT EXISTS '" . DB::USER . "'@'" . DB::HOST . "' IDENTIFIED by '" . DB::PASS . "';";
-$usage = "GRANT USAGE ON *.* TO '" . DB::USER . "'@'" . DB::HOST . "' require_once NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;";
+$usage = "GRANT USAGE ON *.* TO '" . DB::USER . "'@'" . DB::HOST . "' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;";
 $db = "CREATE DATABASE IF NOT EXISTS `" . DB::NAME . "`;";
 $privileges = "GRANT ALL PRIVILEGES ON `" . DB::NAME . "`.* TO '" . DB::USER . "'@'" . DB::HOST . "';";
 $use = "USE `" . DB::NAME . "`;";
