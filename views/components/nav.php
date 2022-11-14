4745f9522a84fc3b27c2b2492a2dcc71c7f3e70d
Martín Stanicio <martuytimo@gmail.com>
2022-11-13 21:38:37 -0300

    fix (eliminar clase en desuso)

--- views/components/nav.php
+++ views/components/nav.php
@@ -3,7 +3,7 @@
         <img src="./img/logo.svg" alt="logo" />
     </a>
 
-    <ul class="links">
+    <ul>
         <li><a href="index.php">Inicio</a></li>
         <li><a href="index.php?action=2022">2022</a></li>
         <li><a href="index.php?action=pilotos">Pilotos</a></li>
