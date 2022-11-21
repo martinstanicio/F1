a7ac7962b6022f195c1f0a08c501f26664191915
Martín Stanicio <martuytimo@gmail.com>
2022-11-20 22:06:43 -0300

    añadir etiqueta de viewport

--- views/template.php
+++ views/template.php
@@ -5,6 +5,7 @@
     <title>Formula 1</title>
     <link rel="stylesheet" href="styles/main.css" />
     <link rel="icon" type="image/png" href="img/logo.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
